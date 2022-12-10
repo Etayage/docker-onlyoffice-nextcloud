adee5af026febd9723fc511569966d1635f45898
Aurélien Lemaire <aurelien@etayage.fr>
2022-12-10 15:02:23 +0100

    [MAJ] ajout automatisation install depuis pltf amazon et config base mysql

---
+++ install_amazon_nc-oo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# Un script pour initialiser une instance amazon avec l'image docker NC+OO 
+
+sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get update
+
+sudo apt-get install --yes \
+   ca-certificates \
+   curl \
+   gnupg \
+   lsb-release    
+
+sudo mkdir -p /etc/apt/keyrings     
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg      
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+sudo apt-get update
+
+sudo apt-get install --yes docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose   
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgrp docker 
+
+sudo apt-get install --yes unzip 
+
+wget https://github.com/Etayage/docker-onlyoffice-nextcloud/archive/refs/heads/master.zip
+unzip master.zip
+cd docker-onlyoffice-nextcloud-master
+sudo docker compose up
