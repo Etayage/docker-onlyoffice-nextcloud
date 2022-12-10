018c6ecf8dce4c97714f29d8cd8c669a2c10fae0
Aurélien Lemaire <aurelien@etayage.fr>
2022-12-10 16:21:51 +0100

    [MAJ] add autoconfig file

---
+++ autoconfig.php
@@ -0,0 +1,12 @@
+<?php
+$AUTOCONFIG = array(
+  "dbtype"        => "mysql",
+  "dbname"        => "nextcloud",
+  "dbuser"        => "nextcloud",
+  "dbpass"        => "N3xtCl0ud!",
+  "dbhost"        => "mariadb",
+  "dbtableprefix" => "",
+  "adminlogin"    => "admin",
+  "adminpass"     => "admin_test",
+  "directory"     => "/www/htdocs/nextcloud/data",
+);
